857c45a13fd5bee91c1562cf33f4e92126b9f081
Mokaffe <903064667@qq.com>
2018-07-17 09:18:11 +0800

    learn details about @classmethod

---
+++ constructor/multiple_constructor.py
@@ -0,0 +1,17 @@
+import time
+
+
+class dou:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    @classmethod
+    def today(cls):
+        t = time.localtime()
+        return cls(t.tm_year, t.tm_mon, t.tm_mday)
+
+
+if __name__ == "__main__":
+    b = dou.today()
--- magic_python/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-使用魔术方法丰富Python Class: https://www.u3v3.com/ar/1364
-(这个里面有提到rollback的操作)
-
-"""
